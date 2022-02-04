086411f9e9a15a1e6b0a6dde029491042520806c
Kristrún Jóhannsdóttir <krijo@itu.dk>
2022-02-04 12:32:17 +0100

    Something

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Sidebar';
+import { Component } from 'react/cjs/react.production.min';
+import React from "react"; 
 
-function App() {
+class App extends Component() {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <><h1>Hello</h1><Sidebar></Sidebar></>
+  )
+  }
 }
 
 export default App;
